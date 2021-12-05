b7c4370009d8f8182c3e2a747b95b9a1ec8be02f
RAM111111 <r.r.99@windowslive.com>
2021-12-05 15:24:45 +0300

    Initial Commit

---
+++ TTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTT
+//
+//  Created by R on 01/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
